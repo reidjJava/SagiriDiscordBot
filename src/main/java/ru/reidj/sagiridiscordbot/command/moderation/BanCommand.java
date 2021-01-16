13e0304b9294d3108c9183a503a08029e5354c42
Reidj3 <sp1.ke@mail.ru>
2021-01-17 03:21:37 +0500

    Update BanCommand.java

--- src/main/java/ru/reidj/sagiridiscordbot/command/moderation/BanCommand.java
+++ src/main/java/ru/reidj/sagiridiscordbot/command/moderation/BanCommand.java
@@ -14,8 +14,10 @@ public class BanCommand extends ListenerAdapter {
     public void onMessageReceived(MessageReceivedEvent e) {
         val message = e.getMessage();
         val channel = e.getChannel();
+        val guild = e.getGuild();
+        String[] messages = e.getMessage().getContentRaw().split(" ");
 
-        if (message.getContentRaw().startsWith("!ban")) {
+        /*if (message.getContentRaw().startsWith("!ban")) {
             channel.deleteMessageById(message.getId()).queue();
             if (Objects.requireNonNull(e.getMember()).hasPermission(Permission.BAN_MEMBERS)) {
                 for (Member member : e.getMessage().getMentionedMembers()) {
@@ -23,6 +25,20 @@ public class BanCommand extends ListenerAdapter {
                     member.ban(1).queue();
                 }
             }
+        }*/
+
+        try {
+            if (messages[0].equalsIgnoreCase("!ban")) {
+                if (Objects.requireNonNull(e.getMember()).hasPermission(Permission.BAN_MEMBERS)) {
+                    channel.deleteMessageById(message.getId()).queue();
+                    for (Member member : e.getMessage().getMentionedMembers()) {
+                        channel.sendMessage(member.getAsMention() + " был(а) заблокирован(а) на " + messages[2] + " день(ней)!").queue();
+                        member.ban(Integer.parseInt(messages[2])).complete();
+                    }
+                }
+            }
+        } catch (ArrayIndexOutOfBoundsException exception) {
+            channel.sendMessage("Синтаксическая ошибка! Используйте !mute @имя время(в секундах)").queue();
         }
     }
 }
